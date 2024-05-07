72081ee627fa902001e123d1532db45e2606a888
Ethan <eclark715@gmail.com>
2024-05-07 15:42:13 -0700

    Updated arguments

--- bioinformatics-sota-eval/arguments.py
+++ bioinformatics-sota-eval/arguments.py
@@ -33,7 +33,7 @@ def get_arguments():
         type=str,
         nargs='+',
         help='Models to use',
-        default=['pgnn'],
+        default=['ann', 'gnn', 'pgnn', 'megagnn', 'kpnn'],
     )
     
     parser.add_argument(
